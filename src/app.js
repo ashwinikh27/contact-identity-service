54e8c6cc4606a33589a46256a2b83951eb344499
ashwini27 <ashwinikh027@gmail.com>
2026-03-06 19:01:27 +0530

    Prepare project for deployment

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.get("/", (req, res) => {
   res.send("Identity Reconciliation API Running");
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
